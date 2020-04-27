e248dec0535783f402431e8cb3757bb35ced134c
cfg5099 <cfg5099@gmail.com>
2020-04-27 14:44:22 -0400

    simple version that encrypts only. Doesn't fully work, need to add a way to control the restart of the alphabet

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello")
+print("Please enter the phrase to be encrypted: ")
+phrase = list(input())
+
+print("Please enter the key:")
+key = input()
+key = int(key)
+
+if(key > 25):
+	key = key % 26;
+
+for x in range(0, len(phrase)):
+	charNum  = ord(phrase[x])
+	newchar = chr(charNum + key)
+	phrase[x] = newchar
+	pass
+
+newPhrase = "".join(phrase)
+print("The new encrypted phrase is: " + newPhrase)
